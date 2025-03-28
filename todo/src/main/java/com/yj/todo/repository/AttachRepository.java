aeac0caeb84b2091be3ce977f4ee8d9c51ba4138
devyej8753 <devyej8753@gmail.com>
2025-03-28 16:09:37 +0900

    [lyj] file upload

---
+++ todo/src/main/java/com/yj/todo/repository/AttachRepository.java
@@ -0,0 +1,9 @@
+package com.yj.todo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.yj.todo.entity.Attach;
+
+public interface AttachRepository extends JpaRepository<Attach, Long>{
+
+}
