a619bc425f51d26e687eadb75736964653cd109d
devyej8753 <devyej8753@gmail.com>
2025-04-02 15:32:38 +0900

    Merge branch 'master' of https://github.com/devyej8753/todo_project.git

--- todo/src/main/java/com/yj/todo/controller/AttachController.java
+++ todo/src/main/java/com/yj/todo/controller/AttachController.java
@@ -65,7 +65,7 @@ public class AttachController {
 	// 파일 다운로드
 	@GetMapping("/download/{id}")
 	public ResponseEntity<Object> fileDownload(@PathVariable("id") Long id){
-		System.out.println("ci/cd test8");
+		System.out.println("ci/cd test9");
 		try {
 			Attach fileData = attachService.selectAttachOne(id);
 			if(fileData == null) {
